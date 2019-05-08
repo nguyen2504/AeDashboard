724c3000fcc8d27b4ea3237383724a351c1133eb
Nguyen <nguyen.nguyen@dmttek.com>
2019-05-08 09:55:05 +0700

    api

---
+++ TestApi/TestApi/Controllers/TestApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+    }
+}
